154768bfbd942fd9abfa30814bc066d34eb51ac5
borders0 <aguy@borders0.com>
2025-10-01 17:38:31 -0600

    Update Hashes

--- Formula/connector.rb
+++ Formula/connector.rb
@@ -1,9 +1,9 @@
 class Connector < Formula
   desc "Installation of git connector"
   homepage "https://github.com/borders0/homebrew-sec-connector"
-  url "https://github.com/borders0/homebrew-sec-connector/archive/refs/tags/v2.1.5.tar.gz"
+  url "https://github.com/borders0/homebrew-sec-connector/archive/refs/tags/v1.8.2.tar.gz"
   version "1.8.2"
-  sha256 "4fc4c2c4c67447b099a887621cfe47657bda5e7ce11233855142eb97ece5c795"
+  sha256 "d8471f31366a0c367b03ba164e190285e47f61549e488e0eca416dfb80e3c85b"
   license "MIT"
 
    def install
